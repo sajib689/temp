2e80477b49e5b32d59a73c9cc609cf6f714113af
sajib689 <sajibbabu751@gmail.com>
2022-09-05 13:02:12 +0600

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const API_KEY = `62369acb191530613d24eaa9cdcea49d`
+const loadTemperature = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+fetch (url)
+.then (res => res.json())
+.then (data => displayTemperature(data))
+}
+const displayTemperature = data => {
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('cloud', data.weather[0].main)
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchFiled = document.getElementById('search-filed');
+    const city = searchFiled.value;
+    document.getElementById('city').innerText = city;
+    loadTemperature(city)
+})
+loadTemperature('dhaka')
